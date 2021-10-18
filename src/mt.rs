6d2ab20c692e4d46af55b4dff7e146ebf767fd1f
Tiago MARTINEZ <tiago.martinez@ingenico.com>
2021-10-18 15:35:05 -0300

    Change MT constants to actual Rust `const`

--- src/mt.rs
+++ src/mt.rs
@@ -1,44 +1,32 @@
 /// A simplist Mersenne Twister implementation in Rust
+#[derive(Debug)]
 pub struct Mt19937 {
-    w: u32,
-    n: u32,
-    m: u32,
-    a: u32,
-    u: u32,
-    d: u32,
-    s: u32,
-    b: u32,
-    t: u32,
-    c: u32,
-    f: u32,
-
     mt: Vec<u32>,
     index: usize,
-    lower_mask: u32,
-    upper_mask: u32,
 }
+
+pub const W: u32 = 32;
+pub const N: u32 = 624;
+pub const M: u32 = 397;
+pub const A: u32 = 0x9908B0DF;
+pub const U: u32 = 11;
+pub const S: u32 = 7;
+pub const B: u32 = 0x9D2C5680;
+pub const T: u32 = 15;
+pub const C: u32 = 0xEFC60000;
+pub const F: u32 = 1812433253;
+pub const L: u32 = 18;
+
+const LOWER_MASK: u32 = (1 << 31) - 1;
+const UPPER_MASK: u32 = 1 << 31;
 
 impl Mt19937 {
 
     /// Return a new Mt19937 instance *without* seed.
     pub fn new() -> Mt19937 {
         Mt19937 {
-            w: 32,
-            n: 624,
-            m: 397,
-            a: 0x9908B0DF,
-            u: 11,
-            d: 0xFFFFFFFF,
-            s: 7,
-            b: 0x9D2C5680,
-            t: 15,
-            c: 0xEFC60000,
-            f: 1812433253,
-
             mt: vec![0; 624],
             index: 625,
-            lower_mask: (1 << 31) - 1,
-            upper_mask: (1 << 31),
         }
     }
 
@@ -49,43 +37,52 @@ impl Mt19937 {
         mt
     }
 
+    pub fn with_state(state: Vec<u32>) -> Mt19937 {
+        assert_eq!(state.len(), 624);
+        Mt19937 {
+            mt: state,
+            index: 624,
+        }
+    }
+
     /// Re-seed this instance with `seed`.
     pub fn seed(&mut self, seed: u32) {
-        self.index = self.n as usize;
+        self.index = N as usize;
         self.mt[0] = seed;
-        for i in 1..self.n as usize {
-            self.mt[i] = self.f.wrapping_mul(self.mt[i - 1] ^ (self.mt[i - 1] >> (self.w - 2))).wrapping_add(i as u32);
+        for i in 1..N as usize {
+            self.mt[i] = F.wrapping_mul(self.mt[i - 1] ^ (self.mt[i - 1] >> (W - 2))).wrapping_add(i as u32);
         }
     }
 
     /// Return the next 32-bit number.
     pub fn next(&mut self) -> u32 {
-        if self.index >= self.n as usize {
-            if self.index > self.n as usize {
+        if self.index >= N as usize {
+            if self.index > N as usize {
                 self.seed(5489);
             }
             self.twist();
         }
 
         let mut y = self.mt[self.index];
-        y ^= (y >> self.u) & self.d;
-        y ^= (y << self.s) & self.b;
-        y ^= (y << self.t) & self.c;
-        y ^= y >> 1;
-
         self.index += 1;
+
+        y ^= y >> U;
+        y ^= (y << S) & B;
+        y ^= (y << T) & C;
+        y ^= y >> L;
+
         y
     }
 
     // The twist operation on MT internal state.
     fn twist(&mut self) {
-        for i in 0..self.n as usize {
-            let x = (self.mt[i] & self.upper_mask).wrapping_add(self.mt[(i + 1) % self.n as usize] & self.lower_mask);
+        for i in 0..N as usize {
+            let x = (self.mt[i] & UPPER_MASK).wrapping_add(self.mt[(i + 1) % N as usize] & LOWER_MASK);
             let mut x_a = x >> 1;
             if (x & 1) != 0 {
-                x_a = x_a ^ self.a;
+                x_a = x_a ^ A;
             }
-            self.mt[i] = self.mt[(i + self.m as usize) % self.n as usize] ^ x_a
+            self.mt[i] = self.mt[(i + M as usize) % N as usize] ^ x_a
         }
         self.index = 0;
     }
